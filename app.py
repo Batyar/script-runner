5a8fdcccb6d817d010fad9409503e739d4dce689
Roman <pinchlog1@gmail.com>
2015-10-20 22:27:01 +0300

    Handle permission error and render images

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, flash, redirect, url_for, request, get_flashed_messages
 from flask.ext.login import LoginManager, current_user, login_user, logout_user, login_required
 from flask.ext.bcrypt import check_password_hash
 from models import *
-import os
+import os, re
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -79,24 +79,33 @@ def files():
 @app.route('/files/<path:req_path>')
 @login_required
 def dir_listing(req_path):
-    abs_path = '/' + req_path
-    if not os.path.exists(abs_path):
-        return abort(404)
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
-    return split_folders_and_files(abs_path + '/')
-    
+    try:
+        abs_path = '/' + req_path
+        if not os.path.exists(abs_path):
+            return abort(404)
+        if os.path.isfile(abs_path):
+            return send_file(abs_path)
+        return split_folders_and_files(abs_path + '/')
+    except OSError:
+        flash('Permission denied')
+        return redirect(url_for('files'))
 
 def split_folders_and_files(path):
     arr = sorted(os.listdir(path), key=lambda s: s.lower())
     files, folders = [], []
     for i in arr:
       files.append(i) if os.path.isfile(path + i) else folders.append(i)
-    return render_template('files.html', files=files, folders=folders)
+    return render_template('files.html', files=files, folders=folders, check_img=check_img)
 
 def flash_and_redirect(message):
     flash(message)
     return redirect(url_for('login'))
+
+def check_img(file):
+    if re.compile(r'(\.(?:jpg|jpeg|gif|png)$)').search(file) is not None:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
